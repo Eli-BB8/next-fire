1996c11ebac8e0c1d53d7b8885155f6aa62f4221
Eli-BB8 <elikovner1@gmail.com>
2021-10-18 02:11:09 +0300

    auth display, google

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,20 @@
 import '../styles/globals.css'
-
 import Navbar from '../components/Navbar';
 import { Toaster } from 'react-hot-toast';
+import { UserContext } from '../lib/context';
+
+import { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth'
 
 export default function MyApp({ Component, pageProps }) {
+
+  
+
   return (
-    <>
+    <UserContext.Provider value={{ user: {}, username: 'Eli' }}>
       <Navbar />
       <Component {...pageProps} />
       <Toaster />
-    </>
+    </UserContext.Provider>
   );
 }
--- pages/enter.tsx
+++ pages/enter.tsx
@@ -1,7 +1,47 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-img-element */
+import { useContext } from "react";
+import { UserContext } from "../lib/context";
+import { auth, googleAuthProvider } from "../lib/firebase";
+
 export default function EnterPage({ }) {
+  const { user , username } = useContext(UserContext);
+
   return (
     <main>
-    	<h1>Sign Up</h1>
+      {user ? 
+        !username ? <UsernameForm /> : <SignOutButton />
+        :
+        <SignInButton />
+      }
     </main>
   )
 }
+
+function SignInButton() {
+  const signInWithGooogle = async () => {
+    try {
+      await auth.signInWithPopup(googleAuthProvider);
+    } catch (error) {
+      console.log('error sign in %s', error.message);
+    }
+  };
+
+  return (
+    <button className="btn-google" onClick={signInWithGooogle}>
+      <img src={'/google.png'} /> Sign in with Google
+    </button>
+  )
+};
+
+function SignOutButton() {
+  return (
+    <button onClick={() => auth.signOut()}>Sign Out</button>
+  )
+};
+
+function UsernameForm() {
+  return (
+    <></>
+  )
+};
